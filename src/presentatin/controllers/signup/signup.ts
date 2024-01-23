4edea65de3ccc2aaef769896ac581b575a43c92d
Jhonata Sobral <jhonata.sobral@ms.senac.br>
2024-01-22 21:07:05 -0400

    feat: ensure not empty required field

--- src/presentatin/controllers/signup/signup.ts
+++ src/presentatin/controllers/signup/signup.ts
@@ -1,14 +1,27 @@
+import { MissingParamError } from "../../errors";
+import { badRequest } from "../../helpers/http-helper";
 import { Controller } from "../../protocols/controller";
 import { HttpRequest, HttpResponse } from "../../protocols/http";
 
 export class SignUpController implements Controller {
     handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         try {
-            let response: HttpResponse = { statusCode: 200, body: {} }
-            if(!httpRequest.body.nome) {
-                response.body = {
-                    message: 'O campo nome é obrigatório'
+            const requiredFields = [
+                'name',
+                'email',
+                'password',
+                'passwordConfirmation'
+            ];
+
+            for (const field of requiredFields) {
+                if(!httpRequest.body[field]) {
+                    return new Promise(resolve => resolve(badRequest(new MissingParamError(field))))
                 }
+            }
+
+            let response: HttpResponse = { statusCode: 200, body: {} }
+            if(!httpRequest.body?.nome) {
+                response.body = new MissingParamError('nome')
                 response.statusCode = 400
                 return new Promise((resolve, reject) => {
                     resolve(response)
