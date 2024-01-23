f63aed99c5fae329405fe124e70779bd1617d56f
Jhonata Sobral <jhonata.sobral@ms.senac.br>
2024-01-22 21:06:31 -0400

    test: ensure not empty required field

--- src/presentatin/controllers/signup/signup.spec.ts
+++ src/presentatin/controllers/signup/signup.spec.ts
@@ -1,7 +1,9 @@
+import { MissingParamError } from "../../errors";
 import { SignUpController } from "./signup";
 
 describe('SignUp Controller', () => {
     test('Should return 400 if no name is provided', async () => {
+        // Arrange
         const httpRequest = {
             body: {
                 email: 'any_email@mail.com',
@@ -9,10 +11,70 @@ describe('SignUp Controller', () => {
                 passwordConfirmation: 'any_password'
             }
         }
+        const signUpController = new SignUpController()
 
-        const signUpController = new SignUpController
+        // Act
         const httpResponse = await signUpController.handle(httpRequest)
 
+        //Assert
         expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamError('name'))
+    });
+
+    test('Should return 400 if no email is provided', async () => {
+        // Arrange
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                password: 'any_password',
+                passwordConfirmation: 'any_password'
+            }
+        }
+        const signUpController = new SignUpController()
+
+        // Act
+        const httpResponse = await signUpController.handle(httpRequest)
+
+        //Assert
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamError('email'))
+    });
+
+    test('Should return 400 if no password is provided', async () => {
+        // Arrange
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                email: 'any_email@mail.com',
+                passwordConfirmation: 'any_password'
+            }
+        }
+        const signUpController = new SignUpController()
+
+        // Act
+        const httpResponse = await signUpController.handle(httpRequest)
+
+        //Assert
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamError('password'))
+    });
+
+    test('Should return 400 if no passwordConfirmation is provided', async () => {
+        // Arrange
+        const httpRequest = {
+            body: {
+                name: 'any_name',
+                email: 'any_email@mail.com',
+                password: 'any_password'
+            }
+        }
+        const signUpController = new SignUpController()
+
+        // Act
+        const httpResponse = await signUpController.handle(httpRequest)
+
+        //Assert
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
     });
 });
